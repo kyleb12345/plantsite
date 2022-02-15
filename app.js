1add721d953ecf16ae1ddb22b9604c9921b7dbe3
kyle <bartscholar@gmail.com>
2022-02-14 18:38:54 -0600

    third attempt

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const adminRoutes = require('./routes/admin.routes');
 const cartRoutes = require('./routes/cart.routes');
 const ordersRoutes = require('./routes/orders.routes');
 
+let port = process.env.PORT || 3000;
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -55,7 +57,7 @@ app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
   .then(function () {
-    app.listen(3000);
+    app.listen(port);
   })
   .catch(function (error) {
     console.log('Failed to connect to the database!');
